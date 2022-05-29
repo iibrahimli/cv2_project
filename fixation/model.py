7bae7a15d268bb930d93c778f66955700596fda6
iibrahimli <imranibrahimli98@gmail.com>
2022-05-29 23:54:41 +0200

    finish prototype model

--- fixation/model.py
+++ fixation/model.py
@@ -2,17 +2,51 @@
 Fixation prediction model.
 """
 
+from random import gauss
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
 
 from torchvision import models
 from torchvision.models.feature_extraction import create_feature_extractor
 
 
+def gaussian(window_size: int, sigma: float) -> torch.Tensor:
+    device, dtype = None, None
+    if isinstance(sigma, torch.Tensor):
+        device, dtype = sigma.device, sigma.dtype
+    x = torch.arange(window_size, device=device, dtype=dtype) - window_size // 2
+    if window_size % 2 == 0:
+        x = x + 0.5
+    gauss = torch.exp(-x.pow(2.0) / (2 * sigma**2))
+    return gauss / gauss.sum()
+
+
 class Decoder(nn.Module):
     def __init__(self):
         super().__init__()
+        self.bottleneck_up = nn.Sequential(
+            nn.ConvTranspose2d(1024, 512, 2, 2),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+        )
+        self.decoder_blocks = nn.ModuleList(
+            [
+                nn.Sequential(
+                    nn.Conv2d(n_ch * 2, n_ch, 3, padding=1),
+                    nn.BatchNorm2d(n_ch),
+                    nn.ReLU(inplace=True),
+                    nn.ConvTranspose2d(n_ch, n_ch // 2, 2, 2),
+                    nn.BatchNorm2d(n_ch // 2),
+                    nn.ReLU(inplace=True),
+                )
+                for n_ch in [512, 256, 128, 64]
+            ]
+        )
+        self.out = nn.Sequential(
+            nn.Conv2d(32, 1, 3, padding=2),
+        )
 
     def forward(self, features):
         """
@@ -26,7 +60,13 @@ class Decoder(nn.Module):
         # layer2: [N, 256, 28, 28]
         # layer3: [N, 512, 14, 14]
         # layer4: [N, 1024, 7, 7]
-        pass
+
+        feats = list(features.values())
+        x = self.bottleneck_up(feats[-1])
+        for block, feat in zip(self.decoder_blocks, feats[-2::-1]):
+            x = block(torch.cat([x, feat], dim=1))
+        x = self.out(x)
+        return x
 
 
 class FixNet(nn.Module):
@@ -34,7 +74,7 @@ class FixNet(nn.Module):
     UNet-like model for fixation prediction.
     """
 
-    def __init__(self):
+    def __init__(self, center_bias_path, freeze_encoder=False):
         super().__init__()
 
         # encoder is a pretrained resnext50
@@ -49,9 +89,31 @@ class FixNet(nn.Module):
         self.encoder = create_feature_extractor(backbone, feature_nodes)
         self.decoder = Decoder()
 
+        # non-trainable gaussian smoothing kernel
+        g = gaussian(25, 11.2)
+        gaussian_kernel = torch.matmul(g.unsqueeze(-1), g.unsqueeze(-1).t())
+        gaussian_kernel = gaussian_kernel.view(1, 1, 25, 25)
+        self.smoothing_kernel = nn.Parameter(gaussian_kernel, requires_grad=False)
+
+        # center bias
+        center_bias_arr = np.load(center_bias_path)
+        center_bias_log = np.log(center_bias_arr / center_bias_arr.sum())
+        self.center_bias = nn.Parameter(
+            torch.from_numpy(center_bias_log), requires_grad=False
+        )
+
+        if freeze_encoder:
+            for param in self.encoder.parameters():
+                param.requires_grad = False
+
     def forward(self, x):
         feats = self.encoder(x)
-        for k, v in feats.items():
-            print(k, v.shape)
         x = self.decoder(feats)
+
+        # apply gaussian smoothing
+        x = F.conv2d(x, self.smoothing_kernel, padding=11)
+
+        # add center bias
+        x = x + self.center_bias
+
         return x
